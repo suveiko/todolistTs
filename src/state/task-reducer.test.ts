ff814de7e182b6b3448e6ac1050c0ce243f0ba73
suveiko <suveikosasha@gmail.com>
2022-06-14 17:27:00 +0400

    Created test and action for ADD-TASK

--- src/state/task-reducer.test.ts
+++ src/state/task-reducer.test.ts
@@ -1,6 +1,6 @@
 import {v1} from "uuid";
 import {TasksStateType} from "../App";
-import {RemoveTaskAC, taskReducer} from "./task-reducer";
+import {AddTaskAC, RemoveTaskAC, taskReducer} from "./task-reducer";
 
 test('correct task should be removed', () => {
     const todolistId1 = v1()
@@ -28,4 +28,34 @@ test('correct task should be removed', () => {
     const endState = taskReducer(startState,RemoveTaskAC(todolistId1, taskId))
 
     expect(endState[todolistId1][0].title).toBe('JS')
+    expect(endState[todolistId1].length).toBe(4)
+})
+
+test('correct task should be added', () => {
+    const todolistId1 = v1()
+    const todolistId2 = v1()
+
+    const newTaskTitle = 'Angular'
+
+    const startState: TasksStateType = {
+        [todolistId1]: [
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistId2]: [
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+            {id: v1(), title: "ReactJS2", isDone: false},
+            {id: v1(), title: "Rest API2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false},
+        ]
+    }
+
+    const endState = taskReducer(startState,AddTaskAC(todolistId1, newTaskTitle))
+
+    expect(endState[todolistId1][0].title).toBe('Angular')
+    expect(endState[todolistId1].length).toBe(6)
 })
--- src/state/task-reducer.ts
+++ src/state/task-reducer.ts
@@ -1,18 +1,39 @@
 import {TasksStateType} from "../App";
+import {v1} from "uuid";
 
 type RemoveTaskActionType = {
     type: 'REMOVE-TASK'
     id: string
     todoId: string
 }
+type AddTaskActionType = {
+    type: 'ADD-TASK'
+    todoId: string
+    title: string
+}
+type ActionType = RemoveTaskActionType | AddTaskActionType
 
-export const taskReducer = (state: TasksStateType, action: RemoveTaskActionType): TasksStateType => {
+
+export const taskReducer = (state: TasksStateType, action: ActionType): TasksStateType => {
     switch (action.type) {
         case 'REMOVE-TASK':
             return {...state, [action.todoId]: state[action.todoId].filter(t => t.id !== action.id)}
+        case 'ADD-TASK':
+            return {
+                ...state,
+                [action.todoId]:
+                    [
+                        {id: v1(), title: action.title, isDone: false},
+                        ...state[action.todoId]
+                    ]
+            }
+
     }
 }
 
 export const RemoveTaskAC = (todoId: string, id: string): RemoveTaskActionType => {
     return {type: 'REMOVE-TASK', todoId: todoId, id: id}
+}
+export const AddTaskAC = (todoId: string, title: string): AddTaskActionType => {
+    return {type: 'ADD-TASK', todoId: todoId, title: title}
 }

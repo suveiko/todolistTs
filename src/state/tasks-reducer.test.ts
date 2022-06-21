68bbd60aca1df8de486347d199aabcc9bc64632b
suveiko <suveikosasha@gmail.com>
2022-06-21 12:45:43 +0400

    refactor: move 'new array should be added when new todolist is added' test from tasks-reducer.test.ts to todolist-tasks-reducer.test.ts

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -6,7 +6,6 @@ import {
     addTaskAC, changeTaskStatusAC, changeTaskTitleAC,
     removeTaskAC, tasksReducer
 } from "./tasks-reducer";
-import {addTodoListAC} from "./todolists-reducer";
 
 
 test('correct task should be removed', () => {
@@ -125,31 +124,4 @@ test('task title should be changed', () => {
     expect(endState[todolistId2].length).toBe(5)
     expect(endState[todolistId1][3].title).toBe('Rest API')
 })
-test('new array should be added when new todolist is added', () => {
-    const startState: TasksStateType = {
-        'todolistId1': [
-            {id: '1', title: 'CSS', isDone: false},
-            {id: '2', title: 'JS', isDone: true},
-            {id: '3', title: 'React', isDone: false}
-        ],
-        'todolistId2': [
-            {id: '1', title: 'bread', isDone: false},
-            {id: '2', title: 'milk', isDone: true},
-            {id: '3', title: 'tea', isDone: false}
-        ]
-    }
 
-    const action = addTodoListAC('new todolist')
-
-    const endState = tasksReducer(startState, action)
-
-
-    const keys = Object.keys(endState)
-    const newKey = keys.find(k => k !== 'todolistId1' && k !== 'todolistId2')
-    if (!newKey) {
-        throw Error('new key should be added')
-    }
-
-    expect(keys.length).toBe(3)
-    expect(endState[newKey]).toEqual([])
-})
---
+++ src/state/todolist-tasks-reducer.test.ts
@@ -0,0 +1,32 @@
+import {TasksStateType} from "../App";
+import {addTodoListAC} from "./todolists-reducer";
+import {tasksReducer} from "./tasks-reducer";
+
+test('new array should be added when new todolist is added', () => {
+    const startState: TasksStateType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
+
+    const action = addTodoListAC('new todolist')
+
+    const endState = tasksReducer(startState, action)
+
+
+    const keys = Object.keys(endState)
+    const newKey = keys.find(k => k !== 'todolistId1' && k !== 'todolistId2')
+    if (!newKey) {
+        throw Error('new key should be added')
+    }
+
+    expect(keys.length).toBe(3)
+    expect(endState[newKey]).toEqual([])
+})

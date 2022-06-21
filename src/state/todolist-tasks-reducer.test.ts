d26ced22107c6c9b0d0480d3b817033ceef4832b
suveiko <suveikosasha@gmail.com>
2022-06-21 12:56:54 +0400

    refactor: move 'ids should be equals' test from todolists-reducer.test.ts to todolist-tasks-reducer.test.ts

--- src/state/todolist-tasks-reducer.test.ts
+++ src/state/todolist-tasks-reducer.test.ts
@@ -1,32 +1,21 @@
-import {TasksStateType} from "../App";
-import {addTodoListAC} from "./todolists-reducer";
+import {TasksStateType, TodoListsType} from "../App";
+
+import {addTodoListAC, todoListsReducer} from "./todolists-reducer";
 import {tasksReducer} from "./tasks-reducer";
 
-test('new array should be added when new todolist is added', () => {
-    const startState: TasksStateType = {
-        'todolistId1': [
-            {id: '1', title: 'CSS', isDone: false},
-            {id: '2', title: 'JS', isDone: true},
-            {id: '3', title: 'React', isDone: false}
-        ],
-        'todolistId2': [
-            {id: '1', title: 'bread', isDone: false},
-            {id: '2', title: 'milk', isDone: true},
-            {id: '3', title: 'tea', isDone: false}
-        ]
-    }
+test('ids should be equals', () => {
+    const startTasksState: TasksStateType = {}
+    const startTodoListsState: TodoListsType[] = []
 
     const action = addTodoListAC('new todolist')
 
-    const endState = tasksReducer(startState, action)
+    const endTasksState = tasksReducer(startTasksState, action)
+    const endTodoListsState = todoListsReducer(startTodoListsState, action)
 
+    const keys = Object.keys(endTasksState)
+    const idFromTasks = keys[0]
+    const idFromTodoLists = endTodoListsState[0].id
 
-    const keys = Object.keys(endState)
-    const newKey = keys.find(k => k !== 'todolistId1' && k !== 'todolistId2')
-    if (!newKey) {
-        throw Error('new key should be added')
-    }
-
-    expect(keys.length).toBe(3)
-    expect(endState[newKey]).toEqual([])
+    expect(idFromTasks).toBe(action.todoListId)
+    expect(idFromTodoLists).toBe(action.todoListId)
 })
--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -4,8 +4,7 @@ import {
     addTodoListAC, changeTodoListAC, changeTodoListFilterAC,
     removeTodolistAC, todoListsReducer
 } from './todolists-reducer'
-import {FilterValuesType, TasksStateType, TodoListsType} from '../App'
-import { tasksReducer} from "./tasks-reducer";
+import {FilterValuesType,TodoListsType} from '../App'
 
 
 test('correct todolist should be removed', () => {
@@ -68,19 +67,4 @@ test('correct filter of todolist should be changed', () => {
     expect(endState[0].filter).toBe('all')
     expect(endState[1].filter).toBe(newFilter)
 })
-test('ids should be equals', () => {
-    const startTasksState: TasksStateType = {}
-    const startTodoListsState: TodoListsType[] = []
 
-    const action = addTodoListAC('new todolist')
-
-    const endTasksState = tasksReducer(startTasksState, action)
-    const endTodoListsState = todoListsReducer(startTodoListsState, action)
-
-    const keys = Object.keys(endTasksState)
-    const idFromTasks = keys[0]
-    const idFromTodoLists = endTodoListsState[0].id
-
-    expect(idFromTasks).toBe(action.todoListId)
-    expect(idFromTodoLists).toBe(action.todoListId)
-})

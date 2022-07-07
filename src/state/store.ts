7d13a953ef13e0296dfcf0a8618ae22a846a2e6f
suveiko <suveikosasha@gmail.com>
2022-07-07 12:19:11 +0400

    refactor: add DEVTOOLS for store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,4 @@
-import {combineReducers, createStore} from "redux";
+import {combineReducers, compose, legacy_createStore} from "redux";
 import {tasksReducer} from "./tasks-reducer";
 import {todoListsReducer} from "./todolists-reducer";
 
@@ -11,7 +11,13 @@ const rootReducer = combineReducers({
 export type AppRootState = ReturnType<typeof rootReducer>
 
 
-export const store = createStore(rootReducer)
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = legacy_createStore(rootReducer, composeEnhancers());
 
 
 // @ts-ignore

d0be4fb69d0ac0a1cb943f3680ea442b2d3ceacf
suveiko <suveikosasha@gmail.com>
2022-07-12 12:17:23 +0400

    feat: create stories for CheckboxForm

---
+++ src/components/CheckboxForm/CheckboxForm.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import {action} from "@storybook/addon-actions";
+import {CheckboxForm} from "./CheckboxForm";
+
+
+export default {
+    title: 'TODOLIST/Checkbox',
+    component: CheckboxForm,
+    argTypes: {
+        callBack: {
+            description: 'checkbox clicked'
+        }
+    },
+} as ComponentMeta<typeof CheckboxForm>;
+
+const Template: ComponentStory<typeof CheckboxForm> = (args) => <CheckboxForm {...args} />;
+
+export const CheckboxStoriesTrue = Template.bind({});
+CheckboxStoriesTrue.args = {
+    isDone: true,
+    callBack: action('checkbox isDone true')
+};
+
+export const CheckboxStoriesFalse = Template.bind({});
+CheckboxStoriesFalse.args = {
+    isDone: true,
+    callBack: action('checkbox isDone false')
+};
